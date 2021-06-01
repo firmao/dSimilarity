e63f314f49a666e50bcd5df369361badde548cf1
Andre Valdestilhas <firmao@gmail.com>
2021-06-01 01:39:57 -0300

    Add files via upload

--- src/main/java/com/relod/servlet/GenericSparqlServlet.java
+++ src/main/java/com/relod/servlet/GenericSparqlServlet.java
@@ -2,13 +2,16 @@ package com.relod.servlet;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
 import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
@@ -18,12 +21,18 @@ import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.servlet.matching.Util;
+import web.servlet.matching.WIMUDataset;
 
 /**
  * Servlet implementation class SimilarityServlet
@@ -46,28 +55,28 @@ public class GenericSparqlServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		if (request.getParameter("dataset") != null) {
+			Map<String, Set<String>> ret = new HashMap<String, Set<String>> ();
 			String endPoint = request.getParameter("dataset");
 			String query = request.getParameter("query");
-			// String strQuery = "SELECT * where {?s ?p ?o} limit 10" ;
-//			if (endPoint.indexOf("sparql") > 0) {
-				SPARQLRepository repo = new SPARQLRepository(endPoint);
-				RepositoryConnection conn = repo.getConnection();
-				try {
-					TupleQuery tQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, query);
-					TupleQueryResult rs = tQuery.evaluate();
-					response.getWriter().append("Results:\n");
-					while (rs.hasNext()) {
-						response.getWriter().append(rs.next().toString()).append("\n");
-					}
-					// return Long.parseLong(rs.next().getValue("objs").stringValue());
-				} finally {
-					conn.close();
-				}
-//			} else {
-//				response.getWriter().append(Util.execQueryRDFRes(query, endPoint, -1).toString());
+//			SPARQLRepository repo = new SPARQLRepository(endPoint);
+//			RepositoryConnection conn = repo.getConnection();
+//			try {
+//				TupleQuery tQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, query);
+//				TupleQueryResult rs = tQuery.evaluate();
+//				//response.getWriter().append("Results:\n");
+//				Set<String> setRet = new LinkedHashSet<String>();
+//				while (rs.hasNext()) {
+//					setRet.add(rs.next().toString());
+//				}
+//				ret.put(endPoint, setRet);
+//			} finally {
+//				conn.close();
 //			}
-			response.getWriter().append("\n\nDataset: ").append(request.getParameter("dataset"));
-			response.getWriter().append("\nQuery: ").append(request.getParameter("query"));
+			ret.put(endPoint, execSparql(query, endPoint));
+			Gson gson = new Gson();
+			Type gsonType = new TypeToken<HashMap>(){}.getType();
+	        String gsonString = gson.toJson(ret,gsonType);
+	        response.getWriter().append(gsonString);
 		} else if (request.getParameter("datasets") != null) {
 			Map<String, Set<String>> ret = null;
 			String datasets = request.getParameter("datasets");
@@ -78,12 +87,13 @@ public class GenericSparqlServlet extends HttpServlet {
 					setDs.add(p.trim());
 				}
 				try {
-					
+
 					ret = generateDatasetSimilarity(setDs);
 					Gson gson = new Gson();
-			        Type gsonType = new TypeToken<HashMap>(){}.getType();
-			        String gsonString = gson.toJson(ret,gsonType);
-			        response.getWriter().append(gsonString);
+					Type gsonType = new TypeToken<HashMap>() {
+					}.getType();
+					String gsonString = gson.toJson(ret, gsonType);
+					response.getWriter().append(gsonString);
 //			        int nProps = 0;
 //					for (Map.Entry<String, Set<String>> entry : ret.entrySet()) {
 //						response.getWriter().append("\nDataset pair: " + entry.getKey());
@@ -94,8 +104,7 @@ public class GenericSparqlServlet extends HttpServlet {
 //								.append("" + nProps);
 //						//System.out.println(entry.getKey() + ":" + entry.getValue());
 //				    }
-					
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -114,21 +123,42 @@ public class GenericSparqlServlet extends HttpServlet {
 	}
 
 	public static Map<String, Set<String>> generateDatasetSimilarity(Set<String> datasets) {
-		Map<String, Set<String>> mapExactMatch = null;
+		Map<String, Set<String>> mapExactMatch = new LinkedHashMap<String, Set<String>>();
 		String[] array = datasets.stream().toArray(String[]::new);
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i; j < array.length; j++) {
 				try {
 					if (array[i].equalsIgnoreCase(array[j]))
 						continue;
-					mapExactMatch = getExactMatches(array[i], array[j]);
+					mapExactMatch.putAll(getExactMatches(array[i], array[j]));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 
 		}
+		if(datasets.size() > 1) {
+			return getOnlyMatchProps(datasets, mapExactMatch);
+		}
 		return mapExactMatch;
+	}
+
+	private static Map<String, Set<String>> getOnlyMatchProps(Set<String> datasets,
+			Map<String, Set<String>> mapExactMatch) {
+		Map<String, Set<String>> ret = new LinkedHashMap<String, Set<String>>();
+		ret.put(datasets.toString(), new HashSet<String>());
+		Set<String> sRetain = new HashSet<String>();
+		for (Map.Entry<String, Set<String>> entry : mapExactMatch.entrySet()) {
+			sRetain.addAll(entry.getValue());
+			sRetain.retainAll(entry.getValue());
+//			for(String s: entry.getValue()) {
+//				if(!sRetain.add(s)) {
+//					ret.get(datasets.toString()).add(s);
+//				}
+//			}
+		}
+		ret.get(datasets.toString()).addAll(sRetain);
+		return ret;
 	}
 
 	private static Map<String, Set<String>> getExactMatches(String source, String target)
@@ -190,24 +220,52 @@ public class GenericSparqlServlet extends HttpServlet {
 	private static Set<String> execSparql(String cSparql, String source) {
 		final Set<String> ret = new LinkedHashSet<String>();
 		try {
-//			if (source.indexOf("sparql") > 0) {
+			if (source.indexOf("sparql") > 0) {
 				SPARQLRepository repo = new SPARQLRepository(source);
 				RepositoryConnection conn = repo.getConnection();
 				try {
 					TupleQuery tQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, cSparql);
 					TupleQueryResult rs = tQuery.evaluate();
 					while (rs.hasNext()) {
-						ret.add(rs.next().toString());
+						ret.add(rs.next().toString().replaceAll("p=", "").replace("[", "").replace("]", ""));
 					}
 				} finally {
 					conn.close();
 				}
-//			} else {
-//				ret.addAll(Util.execQueryRDFRes(cSparql, source, -1));
-//			}
+			} else {
+				// ret.addAll(Util.execQueryRDFRes(cSparql, source, -1));
+				String sJson = execSparqlFile(cSparql, source);
+				sJson = sJson.replaceAll("=p", "");
+				Type mapType = new TypeToken<Map<String, Set<String>>>(){}.getType();  
+				Map<String, Set<String>> son = new Gson().fromJson(sJson, mapType);
+				for (Map.Entry<String, Set<String>> entry : son.entrySet()) {
+//					for(String s: entry.getValue()) {
+//						ret.add("[" + s + "]");
+//					}
+					ret.addAll(entry.getValue());
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return ret;
+	}
+
+	private static String execSparqlFile(String cSparql, String source) throws InterruptedException, IOException {
+		String sJson = null;
+
+		String nURI = URLEncoder.encode(cSparql, "UTF-8");
+
+		URL urlSearch = new URL(
+				"http://141.57.11.86:8082/DatasetMatchingWeb/sparqlservlet?dataset=" + source + "&query=" + nURI);
+		InputStreamReader reader = null;
+		try {
+			reader = new InputStreamReader(urlSearch.openStream());
+		} catch (Exception e) {
+			Thread.sleep(5000);
+			reader = new InputStreamReader(urlSearch.openStream());
+		}
+		sJson = IOUtils.toString(reader);
+		return sJson;
 	}
 }
